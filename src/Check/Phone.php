d6ac588977ff61867b935db94918c6c923252097
Aleksey Ilyin <alksily@icloud.com>
2019-03-07 16:39:30 +0200

    fix phone check

--- src/Check/Phone.php
+++ src/Check/Phone.php
@@ -6,7 +6,7 @@ use AEngine\Validator\FilterRule;
 
 class Phone extends FilterRule
 {
-    protected $pattern = '\(?\+[0-9]{1,3}\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})? ?(\w{1,10}\s?\d{1,6})?';
+    protected $pattern = '/\(?\+[0-9]{1,3}\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})? ?(\w{1,10}\s?\d{1,6})?/';
 
     /**
      * Validates that the value is phone
